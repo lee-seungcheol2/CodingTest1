dfa3e0414ee2cfe38c0f1f454c8e6e031de297e2
lee-seungcheol2 <dltmdcjfdls@naver.com>
2025-06-03 18:33:29 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1749.54 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
@@ -4,16 +4,34 @@
 using namespace std;
 
 vector<int> solution(vector<int> arr) {
-    if(arr.size() == 1){
-        arr[0] = -1;
-        return arr;
-    }
+    vector<int> answer;
+    vector<int> psuhpush = arr;
+    if (arr.size() < 2)
+{
+	answer = { -1 };
+	return answer;
+}
 
-    int min = 0;
-    for(int i = 1; i < arr.size(); i++){
-        if(arr[i] < arr[min])
-            min = i;
-    }
-    arr.erase(arr.begin()+min);
-    return arr;
+for (int i = 0; i < arr.size(); ++i)
+{
+	for (int j = i + 1; j < arr.size(); ++j)
+	{
+		if (arr.at(i) > arr.at(j))
+		{
+			int Buf = arr.at(i);
+			arr.at(i) = arr.at(j);
+			arr.at(j) = Buf;
+		}
+	}
+}
+for (int i = 0; i < psuhpush.size(); ++i)
+{
+	if (psuhpush.at(i) == arr.at(0))
+		continue;
+
+	answer.push_back(psuhpush.at(i));
+}
+    
+    
+    return answer;
 }
