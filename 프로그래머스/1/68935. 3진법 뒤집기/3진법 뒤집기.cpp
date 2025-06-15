9e4be68a4a882226dbacf803a198519667b9a6d1
lee-seungcheol2 <dltmdcjfdls@naver.com>
2025-06-15 10:08:13 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 3.62 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -2,37 +2,14 @@
 #include <vector>
 
 using namespace std;
-
 int solution(int n) {
-    int answer = 0;
-    vector<int> ans;
-    int i = 0;
-    int b = 0;
-    int m = 3;
-    int j=0;
-    while (n>0) {
-	b = n % 3;
-	n = n / 3;
-	ans.push_back(b);
-	
-	++i;
-	}
-    for (int i = (ans.size() - 1); i >= 0; --i)
-{
-	
-	
-	if (j > 0)
-	{
-		answer+=(ans.at(i) * m);
-		m *= 3;
-	}
-	else
-		answer = answer + ans.at(i);
-	++j;
-		
+    vector<int> a;
+    while (n) {
+        a.push_back(n % 3);
+        n /= 3;
+    }
 
-}
-    
-    
-    return answer;
+    int ret = 0;
+    for (int i = 0; i < a.size(); ++i) ret = ret * 3 + a[i];
+    return ret;
 }
