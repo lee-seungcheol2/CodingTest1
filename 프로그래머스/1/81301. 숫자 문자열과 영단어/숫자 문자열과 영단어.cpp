d3bcae795b89b119d274b64b9967eb4d8acadb46
lee-seungcheol2 <dltmdcjfdls@naver.com>
2025-06-17 14:52:24 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -0,0 +1,89 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    string c = "";
+string b = "";
+
+for(int i=0;i<s.size();++i)
+{
+	if ( 58 > s[i] )
+		c += s[i];
+	else    //3일때
+	{
+		for (int j = i; j < i+2; ++j)
+		{
+			b += s[j];
+		}
+		if (b == "ze")
+		{
+			c += '0';
+			i += 3;
+			b = "";
+		}
+		else if (b == "on")
+		{
+			c += '1';
+			i += 2;
+			b = "";
+		}
+		else if (b == "tw")
+		{
+			c += '2';
+			i += 2;
+			b = "";
+		}
+		else if (b == "th")
+		{
+			c += '3';
+			i += 4;
+			b = "";
+		}
+		else if (b == "fo")
+		{
+			c += '4';
+			i += 3;
+			b = "";
+		}
+		else if (b == "fi")
+		{
+			c += '5';
+			i += 3;
+			b = "";
+		}
+		else if (b == "si")
+		{
+			c += '6';
+			i += 2;
+			b = "";
+		}
+		else if (b == "se")
+		{
+			c += '7';
+			i += 4;
+			b = "";
+		}
+		else if (b == "ei")
+		{
+			c += '8';
+			i += 4;
+			b = "";
+		}
+		else if (b == "ni")
+		{
+			c += '9';
+			i += 3;
+			b = "";
+		}
+	}
+
+}
+int a = 0;
+for (int i = 0; i < c.length(); ++i)
+{
+	a = a * 10 + static_cast<int>(c[i]-48);
+}
+    return a;
+}
