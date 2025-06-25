35956f8333332f7f875a1bcb95ff68e3bd2f59e7
lee-seungcheol2 <dltmdcjfdls@naver.com>
2025-06-25 20:06:40 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 4.27 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -1,3 +1,4 @@
+
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -6,28 +7,13 @@ using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
-    
-    vector<int> ans;
-vector<int> tem;
+    vector<int> temp;
 
-for (int i = 0; i < commands.size(); ++i)
-{
-	for (int j = 0; j < 3; ++j)
-	{
-		ans.push_back(commands[i][j]);
-	}
+    for(int i = 0; i < commands.size(); i++) {
+        temp = array;
+        sort(temp.begin() + commands[i][0] - 1, temp.begin() + commands[i][1]);
+        answer.push_back(temp[commands[i][0] + commands[i][2]-2]);
+    }
 
-	for (int k = ans.at(0)-1; k < ans.at(1); ++k)
-	{
-		
-		tem.push_back(array.at(k));
-	
-	}
-	sort(tem.begin(), tem.end());
-	answer.push_back(tem.at(ans.at(2)-1));
-    tem.clear(); ans.clear();
-}
-    
-    
     return answer;
 }
