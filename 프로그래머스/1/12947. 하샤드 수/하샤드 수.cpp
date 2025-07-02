5444410b4af11f58fb5410b19534dd8a656b9ea0
lee-seungcheol2 <dltmdcjfdls@naver.com>
2025-07-02 12:00:20 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -10,13 +10,11 @@ bool solution(int x) {
 	vector<int> na = {};
 	while (y)
 	{
-		na.push_back(y % 10);
+		a+=(y % 10);
 		y /= 10;
 	}
-	for (int i = 0; i < na.size(); ++i)
-	{
-		a += na.at(i);
-	}
+	
+	
 	answer = x % a == 0 ? true : false;
 
 	return answer;
