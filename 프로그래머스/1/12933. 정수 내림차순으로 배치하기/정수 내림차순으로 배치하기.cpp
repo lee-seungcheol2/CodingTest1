ae1c13017ddc1349c68448286a788d1b456386d1
lee-seungcheol2 <dltmdcjfdls@naver.com>
2025-07-02 11:30:51 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -1,44 +1,33 @@
 #include <string>
 #include <vector>
-#include <iostream>
-
 using namespace std;
-
 long long solution(long long n) {
-    long long answer = 0;
-    int qu=1;
-    vector<int> vArray;
-    
-    for(int i=0;i<10;++i)
-    {
-        if(n)
-        {
-            vArray.push_back(n%10);
-            n/=10;
-        }
-    }
-    for (int i = 0; i < vArray.size(); ++i)
-{
-	
-	for (int j = i; j < vArray.size(); ++j)
-	{ 
-		if (vArray[j] > vArray[i])
+	long long answer = 0;
+	vector<long long> na = {};
+	while (n)
+	{
+		na.push_back(n % 10);
+		n /= 10;
+	}
+
+	for (int i = 0; i < na.size()-1; ++i)
+	{
+		for (int j = 0; j < na.size() - 1-i; ++j)
 		{
-			int buf = vArray[i];
-			vArray[i] = vArray[j];
-			vArray[j] = buf;
+			if (na[j] < na[j + 1])
+			{
+				long long temp = na[j + 1];
+				na[j + 1] = na[j];
+				na[j] = temp;
+			}
 		}
 	}
-	
-}
-  for(int i=1; i<vArray.size();++i)
-	qu *= 10;
-    
-    for (int i = 0; i < vArray.size(); ++i)
-{
-	answer += (vArray[i] * qu);
-	qu /= 10;
-}
-    
-    return answer;
+	long long ret = 0;
+	for (int i = 0; i < na.size(); ++i)
+	{
+		ret = (ret * 10) + na.at(i);
+	}
+
+
+	return ret;
 }
