29d8baae4c2adf50ed0056db3246cda4f5941d6b
lee-seungcheol2 <dltmdcjfdls@naver.com>
2025-07-02 11:57:06 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -1,24 +1,23 @@
 #include <string>
 #include <vector>
-#include <iostream>
+
 using namespace std;
 
 bool solution(int x) {
-    bool answer = true;
-    vector<int> vArray;
-    int sum=0;
-    int rep=x;
-    for(int i=0;i<5;++i)
-    { if(rep){
-    vArray.push_back(rep%10);
-    rep/=10;
-    sum+=vArray[i];
-    }
-    }
-    if(x%sum==0)
-        answer=true;
-    else
-        answer=false;
-    
-    return answer;
+	bool answer = true;
+	int a=0;
+	int y = x;
+	vector<int> na = {};
+	while (y)
+	{
+		na.push_back(y % 10);
+		y /= 10;
+	}
+	for (int i = 0; i < na.size(); ++i)
+	{
+		a += na.at(i);
+	}
+	answer = x % a == 0 ? true : false;
+
+	return answer;
 }
