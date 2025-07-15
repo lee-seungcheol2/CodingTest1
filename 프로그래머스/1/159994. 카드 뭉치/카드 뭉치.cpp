00bc41e8429168489230981ecddce704282131b2
lee-seungcheol2 <dltmdcjfdls@naver.com>
2025-07-15 10:20:09 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -3,18 +3,20 @@
 
 using namespace std;
 
+
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
-    int i = 0, j = 0;
-    for (const auto& word : goal) {
-        if (i < cards1.size() && word == cards1[i]) {
-            i++;
-        }
-        else if (j < cards2.size() && word == cards2[j]) {
-            j++;
-        }
-        else {
-            return "No";
-        }
+  int l = 0;
+  int r = 0;
+
+  for (string& g: goal) {
+    if (g == cards1[l]) {
+      l++;
+    } else if (g == cards2[r]) {
+      r++;
+    } else {
+      return "No";
     }
-    return "Yes";
+  }
+
+  return "Yes";
 }
